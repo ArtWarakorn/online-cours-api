5a7feffef638fd7eab917c97adc8777588433e99
ArtWarakorn <junsongneanart@gmail.com>
2026-04-13 12:25:07 +0700

    post api

---
+++ app/api/users/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { supabase } from "@/lib/supabase";
+
+//get all users
+export async function GET(request) {
+    const { data, error } = await supabase
+        .schema('classroom')
+        .from('users')
+        .select('*');
+
+    if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json(data);
+}
+// POST a new user
+export async function POST(request) {
+    try {
+        const body = await request.json();
+        const { email, password, full_name, role } = body;
+
+        const { data, error } = await supabase
+            .schema('classroom')
+            .from("users")
+            .insert([{ email, password, full_name, role }])
+            .select()
+            .single();
+
+        if (error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
+        }
+
+        return NextResponse.json(data, { status: 201 });
+    } catch (e) {
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
